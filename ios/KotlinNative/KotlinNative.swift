5555975b9c699dfb0c5323c9a76ab9598ca29f3b
felix proehl <felixproehl@outlook.com>
2018-04-01 23:17:21 +0200

    added ios library

---
+++ ios/KotlinNative/KotlinNative.swift
@@ -0,0 +1,11 @@
+//
+//  KotlinNative.swift
+//  KotlinNative
+//
+//  Created by felix proehl on 01.04.18.
+//  Copyright © 2018 The Chromium Authors. All rights reserved.
+//
+
+class KotlinNative {
+
+}
