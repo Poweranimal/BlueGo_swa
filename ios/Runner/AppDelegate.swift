4cd5a6debb953395b4589a5cd310880d68123958
felix proehl <felixproehl@outlook.com>
2018-04-02 13:25:47 +0200

    ITS WORKING!!!!

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import KotlinNative
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    NSLog(KotlinNativeCommon().platform())
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
